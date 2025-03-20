dcf141b742001ca2245ad2fca991deb6fcbd18f5
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-20 21:30:26 +0800

    init

---
+++ src/main/java/dev/xkmc/chunkloadingprofiler/init/ChunkLoadingProfiler.java
@@ -0,0 +1,16 @@
+package dev.xkmc.chunkloadingprofiler.init;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ChunkLoadingProfiler.MODID)
+public class ChunkLoadingProfiler {
+
+	public static final String MODID = "chunkloadingprofiler";
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public ChunkLoadingProfiler() {
+	}
+
+}
